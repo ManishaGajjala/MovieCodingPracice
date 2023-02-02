52d1441dfde0b34eb9fd084d5506df5e68e002dc
root <gmanisahreddy121@gmail.com>
2023-02-02 09:26:37 +0000

    firstAPI

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+const path = require("path");
+const app = express();
+const dbPath = path.join(__dirname, "moviesData.db");
+app.use(express.json());
+let database = null;
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({ filename: dbPath, driver: sqlite3.Database });
+    app.listen(3000, () => {
+      console.log("Server Is running on http://localhost:3000");
+    });
+  } catch (error) {
+    console.log(`Data base Error is ${error}`);
+    process.exit(1);
+  }
+};
+initializeDbAndServer();
+
+//GET movies API
+app.get("/movies/", async (request, response) => {
+  const getMoviesQuery = `
+       SELECT * FROM movie
+        ORDER BY movie_id;
+    `;
+  const movieArray = await database.all(getMoviesQuery);
+  //console.log(movieArray);
+  response.send(movieArray);
+});
